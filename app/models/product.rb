b4825682a70e3b8cea1fe4cdf75bf11d3d9d6de8
steodhiambo <stephenoginga6@gmail.com>
2025-06-25 21:55:44 +0300

    Fix serialization method for product images in SQLite

--- app/models/product.rb
+++ app/models/product.rb
@@ -5,7 +5,7 @@ class Product < ApplicationRecord
   has_many :ads, dependent: :destroy
 
   mount_uploader :image, ImageUploader
-  serialize :image, JSON # If you use SQLite, add this line
+  serialize :image, coder: JSON # If you use SQLite, add this line
 
   validates :title, :brand, :price, :model, presence: true
   validates :description, length: { maximum: 1000, too_long: "%{count} characters is the maximum allowed. "}
